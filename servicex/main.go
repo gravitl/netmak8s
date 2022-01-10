898c5f21378aae3c74cc1dad735088c2dacba8d2
afeiszli <alex.feiszli@gmail.com>
2022-01-10 10:48:54 -0500

    adding nodeport example

--- servicex/main.go
+++ servicex/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"bytes"
-	"strconv"
-	"log"
-	"io/ioutil"
-	"errors"
-	"os"
-	"net/http"
-	"os/exec"
-	"golang.org/x/build/kubernetes/api"
 	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+
+	"golang.org/x/build/kubernetes/api"
 )
 
 //Start MongoDB Connection and start API Request Handler
@@ -32,18 +33,18 @@ func main() {
 }
 
 func getConnDetails() (string, string, string, string) {
-        serverurl := "http://localhost:8081"
-        if os.Getenv("SERVER_API_URL") != "" {
-                serverurl = os.Getenv("SERVER_API_URL")
-        }
-        network := ""
-        if os.Getenv("NETWORK") != "" {
-                network = os.Getenv("NETWORK")
-        }
-        secret := "secretkey"
-        if os.Getenv("SECRET") != "" {
-                secret = os.Getenv("SECRET")
-        }
+	serverurl := "http://localhost:8081"
+	if os.Getenv("SERVER_API_URL") != "" {
+		serverurl = os.Getenv("SERVER_API_URL")
+	}
+	network := ""
+	if os.Getenv("NETWORK") != "" {
+		network = os.Getenv("NETWORK")
+	}
+	secret := "secretkey"
+	if os.Getenv("SECRET") != "" {
+		secret = os.Getenv("SECRET")
+	}
 	apiappend := ""
 	if network == "" {
 		apiappend = "/api/dns"
@@ -55,11 +56,11 @@ func getConnDetails() (string, string, string, string) {
 
 func getNetmakerDNS(serverurl string, apiappend string, secret string) ([]DNSEntry, error) {
 	var dnsentries []DNSEntry
-	req, err := http.NewRequest("GET", serverurl + apiappend, nil)
+	req, err := http.NewRequest("GET", serverurl+apiappend, nil)
 	if err != nil {
 		return dnsentries, err
 	}
-	req.Header.Set("Authorization", "Bearer " + secret)
+	req.Header.Set("Authorization", "Bearer "+secret)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -72,34 +73,34 @@ func getNetmakerDNS(serverurl string, apiappend string, secret string) ([]DNSEnt
 		if err != nil {
 			log.Fatal(err)
 		}
-	        err = json.Unmarshal(bodyBytes,&dnsentries)
+		err = json.Unmarshal(bodyBytes, &dnsentries)
 	} else {
-		return dnsentries, errors.New("Bad response from server "+strconv.Itoa(resp.StatusCode))
+		return dnsentries, errors.New("Bad response from server " + strconv.Itoa(resp.StatusCode))
 	}
 	return dnsentries, err
 }
 
 func getK8SDNS() ([]api.Service, error) {
 	var services []api.Service
-	out, err := exec.Command("kubectl","get","svc","-A","-o","jsonpath={.items}").Output()
-	err = json.Unmarshal(out,&services)
+	out, err := exec.Command("kubectl", "get", "svc", "-A", "-o", "jsonpath={.items}").Output()
+	err = json.Unmarshal(out, &services)
 	return services, err
 }
 
 func compareAndPush(nmdns []DNSEntry, k8sdns []api.Service, serverurl string, network string, secret string, apiappend string) error {
 	var err error
 	for _, service := range k8sdns {
-		dnsname := service.ObjectMeta.Name +"."+service.ObjectMeta.Namespace
+		dnsname := service.ObjectMeta.Name + "." + service.ObjectMeta.Namespace
 		found := false
 		for _, entry := range nmdns {
 			if entry.Name == dnsname {
 				if entry.Address != service.Spec.ClusterIP {
 					entry.Address = service.Spec.ClusterIP
 					err = createUpdateDNS("update",
-								serverurl,
-								"/api/dns/"+network+"/"+entry.Name,
-								secret,
-								entry)
+						serverurl,
+						"/api/dns/"+network+"/"+entry.Name,
+						secret,
+						entry)
 					if err != nil {
 						log.Println(err)
 					}
@@ -110,15 +111,15 @@ func compareAndPush(nmdns []DNSEntry, k8sdns []api.Service, serverurl string, ne
 		}
 		if found == false {
 			newentry := DNSEntry{
-					Address: service.Spec.ClusterIP,
-					Name: dnsname,
-					Network: network,
-				}
+				Address: service.Spec.ClusterIP,
+				Name:    dnsname,
+				Network: network,
+			}
 			err = createUpdateDNS("create",
-						serverurl,
-						"/api/dns/"+network,
-						secret,
-						newentry)
+				serverurl,
+				"/api/dns/"+network,
+				secret,
+				newentry)
 		}
 		if err != nil {
 			log.Println(err)
@@ -132,27 +133,27 @@ func createUpdateDNS(crudop string, serverurl string, apiappend string, secret s
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest("PUT", serverurl + apiappend,  bytes.NewBuffer(entrybytes))
+	req, err := http.NewRequest("PUT", serverurl+apiappend, bytes.NewBuffer(entrybytes))
 	if crudop == "create" {
-		req, err = http.NewRequest("POST", serverurl + apiappend,  bytes.NewBuffer(entrybytes))
+		req, err = http.NewRequest("POST", serverurl+apiappend, bytes.NewBuffer(entrybytes))
 	}
-        if err != nil {
-                return err
-        }
-        req.Header.Set("Authorization", "Bearer " + secret)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+secret)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
-        if err != nil {
-                return err
-        }
-        defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-        if resp.StatusCode != http.StatusOK {
-                err = errors.New("Bad response from server "+strconv.Itoa(resp.StatusCode))
-        }
-	log.Println("pushing",entry)
-        return err
+	if resp.StatusCode != http.StatusOK {
+		err = errors.New("Bad response from server " + strconv.Itoa(resp.StatusCode))
+	}
+	log.Println("pushing", entry)
+	return err
 }
 
 type DNSEntry struct {
@@ -160,5 +161,3 @@ type DNSEntry struct {
 	Name    string `json:"name" bson:"name" validate:"required,name_unique,min=1,max=192"`
 	Network string `json:"network" bson:"network" validate:"network_exists"`
 }
-
-
